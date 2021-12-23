24ff28ce8c7a9ae8331cd93abfc813f68ef98157
Seongyeop Lee <48005965+YiSeongYeop@users.noreply.github.com>
2021-12-23 15:13:30 +0900

    bgimg mobile1

--- js/index.js
+++ js/index.js
@@ -3,5 +3,5 @@ var body = document.getElementsByTagName("body")[0];
 window.onresize = function(event){
 var innerWidth = window.innerWidth;
 //innerWidth <= "800" ? console.log("<800") : console.log(">800");
-innerWidth <= "700" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
+innerWidth <= "1100" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
 }
