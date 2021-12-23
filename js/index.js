d4c4147a5f872594290f9f20029a896788fe799a
Seongyeop Lee <48005965+YiSeongYeop@users.noreply.github.com>
2021-12-23 17:20:49 +0900

    img resize

--- js/index.js
+++ js/index.js
@@ -4,14 +4,12 @@ window.onload = function(){
 var innerWidth = window.innerWidth;
 //innerWidth <= "800" ? console.log("<800") : console.log(">800");
 innerWidth <= "800" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
-innerWidth <= "800" ? document.getElementById("hdn-header-logo").src = "./img/logo2.jpg" : document.getElementById("hdn-header-logo").src = "./img/logo1.jpg";
+//innerWidth <= "800" ? document.getElementById("hdn-header-logo").src = "./img/logo2.jpg" : document.getElementById("hdn-header-logo").src = "./img/logo1.jpg";
 }
 
 window.onresize = function(event){
 var innerWidth = window.innerWidth;
 //innerWidth <= "800" ? console.log("<800") : console.log(">800");
 innerWidth <= "800" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
-innerWidth <= "800" ? document.getElementById("hdn-header-logo").src = "./img/logo2.jpg" : document.getElementById("hdn-header-logo").src = "./img/logo1.jpg";
+//innerWidth <= "800" ? document.getElementById("hdn-header-logo").src = "./img/logo2.jpg" : document.getElementById("hdn-header-logo").src = "./img/logo1.jpg";
 }
-
-document.getElementById("hdn-header-logo").src = "./img/logo2.jpg";
