ae1d709fe7260c2552ffe59da88139bbf1ada0a5
Seongyeop Lee <48005965+YiSeongYeop@users.noreply.github.com>
2021-12-23 16:19:09 +0900

    test

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var body = document.getElementsByTagName("body")[0];
-
+/*
 window.onload = function(){
 var innerWidth = window.innerWidth;
 //innerWidth <= "800" ? console.log("<800") : console.log(">800");
@@ -10,4 +10,21 @@ window.onresize = function(event){
 var innerWidth = window.innerWidth;
 //innerWidth <= "800" ? console.log("<800") : console.log(">800");
 innerWidth <= "800" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
+}
+*/
+window.addEventListener("load", onLoadFunction);
+
+function onLoadFunction(e){
+var innerWidth = window.innerWidth;
+//innerWidth <= "800" ? console.log("<800") : console.log(">800");
+innerWidth <= "800" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
+
+window.addEventListener("resize", onResizeFunction);
+}
+
+function onResizeFunction (e){
+var innerWidth = window.innerWidth;
+//innerWidth <= "800" ? console.log("<800") : console.log(">800");
+innerWidth <= "800" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
+
 }
