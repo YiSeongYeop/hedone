d35d383abe2a82bb29ce5d37507a47856c127749
Seongyeop Lee <48005965+YiSeongYeop@users.noreply.github.com>
2021-12-23 14:53:10 +0900

    imgResize

--- js/index.js
+++ js/index.js
@@ -1,16 +1,7 @@
-function bgimgSize() {
-	const image = document.getElementById("body");
+var body = document.getElementsByTagName("body")[0];
 
-	var bzWidth = window.innerWidth;
-	var bzHeight = window.innerHeight;
-	var bgimgWidth = 3024;
-	var bgimgHeight = 1701;
-
-	if((bzWidth/bzHeight)>=(bgimgWidth/bgimgHeight)) {//화변가로비>=이미지가로비
-
-	}
-	else {//화면가로비<이미지가로비
-
-	}
-
+window.onresize = function(event){
+var innerWidth = window.innerWidth;
+//innerWidth <= "800" ? console.log("<800") : console.log(">800");
+innerWidth <= "700" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
 }
