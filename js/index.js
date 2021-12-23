6897b4e794a3d1cac2a9cbad2b1637cac0160d62
Seongyeop Lee <48005965+YiSeongYeop@users.noreply.github.com>
2021-12-23 15:50:58 +0900

    test

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ var body = document.getElementsByTagName("body")[0];
 window.onload = function(){
 	var innerWidth = window.innerWidth;
 	//innerWidth <= "800" ? console.log("<800") : console.log(">800");
-	innerWidth <= "770" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
+	innerWidth <= "767" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
 }
 
 window.onresize = function(event){
 var innerWidth = window.innerWidth;
 //innerWidth <= "800" ? console.log("<800") : console.log(">800");
-innerWidth <= "770" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
+innerWidth <= "767" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
 }
