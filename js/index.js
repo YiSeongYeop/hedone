843ca01471c960caf8281bf22da498b9e2ea1a83
Seongyeop Lee <48005965+YiSeongYeop@users.noreply.github.com>
2021-12-23 13:54:33 +0900

    bgimgSize

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function bgimgSize() {
+	const image = document.getElementById("body");
+
+	var bzWidth = window.innerWidth;
+	var bzHeight = window.innerHeight;
+	var bgimgWidth = 3024;
+	var bgimgHeight = 1701;
+
+	if((bzWidth/bzHeight)>=(bgimgWidth/bgimgHeight)) {//화변가로비>=이미지가로비
+
+	}
+	else {//화면가로비<이미지가로비
+
+	}
+
+}
