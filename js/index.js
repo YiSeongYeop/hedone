41e206a8a4ff173ed33bd57f50a6be1d37c24e64
Seongyeop Lee <48005965+YiSeongYeop@users.noreply.github.com>
2021-12-23 17:07:21 +0900

    test
    i erase it before but at mobile, logo isn't appear. So add again

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ window.onload = function(){
 var innerWidth = window.innerWidth;
 //innerWidth <= "800" ? console.log("<800") : console.log(">800");
 innerWidth <= "800" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
-innerWidth <= "800" ? document.getElementById("hdn-header-logo").src = "./img/logo3.jpg" : document.getElementById("hdn-header-logo").src = "./img/logo1.jpg";
+innerWidth <= "800" ? document.getElementById("hdn-header-logo").src = "./img/logo2.jpg" : document.getElementById("hdn-header-logo").src = "./img/logo1.jpg";
 }
 
 window.onresize = function(event){
