1d35bd80212c2d2eca657d5ff974b9a8d408aee8
Seongyeop Lee <48005965+YiSeongYeop@users.noreply.github.com>
2021-12-23 16:07:22 +0900

    test

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 var body = document.getElementsByTagName("body")[0];
 
 window.onload = function(){
-	var innerWidth = window.innerWidth;
-	//innerWidth <= "800" ? console.log("<800") : console.log(">800");
-	innerWidth <= "800" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
+var innerWidth = window.innerWidth;
+//innerWidth <= "800" ? console.log("<800") : console.log(">800");
+innerWidth <= "800" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
 }
 
 window.onresize = function(event){
