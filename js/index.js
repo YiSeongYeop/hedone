ff8e0bb58252fa30fa12bdd2a4cc915bc0dfa236
Seongyeop Lee <48005965+YiSeongYeop@users.noreply.github.com>
2021-12-23 15:28:27 +0900

    onload func

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 var body = document.getElementsByTagName("body")[0];
 
+window.onload = function(){
+	var innerWidth = window.innerWidth;
+	//innerWidth <= "800" ? console.log("<800") : console.log(">800");
+	innerWidth <= "700" ? body.style.backgroundImage = "url('./img/main2.jpg')" : body.style.backgroundImage = "url('./img/main.jpg')";
+}
+
 window.onresize = function(event){
 var innerWidth = window.innerWidth;
 //innerWidth <= "800" ? console.log("<800") : console.log(">800");
